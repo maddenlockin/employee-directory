4e855ba8593aa8e4d218bebdb396c0bbc6385ba7
maddenlockin <maddenlockin@gmail.com>
2022-01-07 10:38:07 -0800

    error page added when path does not match established route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from './views/Auth';
+import Error from './views/Error';
 import Home from './views/Home';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Auth />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Auth />} />
+
+        <Route path='*' element={<Error />} />
       </Routes>
     </Router>
   );
---
+++ src/views/Error.jsx
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <div>
+      Error... looks like that page does not exist
+    </div>
+  )
+}
