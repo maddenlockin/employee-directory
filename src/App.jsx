5869113324c0915af51f00ddea29b3bd6c7cab9d
maddenlockin <maddenlockin@gmail.com>
2022-01-09 15:49:27 -0800

    UserContext applied to App and Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
 import Auth from './views/Auth';
 import Error from './views/Error';
 import Home from './views/Home';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Auth />} />
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Auth />} />
 
-        <Route path='*' element={<Error />} />
-      </Routes>
-    </Router>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </UserProvider>
   );
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,8 @@
+import { useUser } from "../context/UserContext";
+
 export default function Home() {
+  const auth = useUser();
+  
   return (
     <h1>HOME</h1>
   )
