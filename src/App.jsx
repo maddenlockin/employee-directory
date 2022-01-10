dc8ff3065635260cd2458622be60f8251953152b
maddenlockin <maddenlockin@gmail.com>
2022-01-09 17:42:14 -0800

    Profile view created and PrivateRoute applied to App for profile route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import { UserProvider } from './context/UserContext';
 import Auth from './views/Auth';
 import Error from './views/Error';
 import Home from './views/Home';
+import Profile from './views/Profile';
 
 export default function App() {
   return (
@@ -11,7 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Auth />} />
-
+          <PrivateRoute path="/profile" element={<Profile />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </Router>
