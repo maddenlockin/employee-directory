c78a1373678c5bd9f744f81b37392a9713009881
maddenlockin <maddenlockin@gmail.com>
2022-01-09 18:07:03 -0800

    Auth view fleshed out more

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,11 @@
 import { useUser } from "../context/UserContext";
+import { Redirect } from "react-router-dom";
 
 export default function Home() {
   const auth = useUser();
-  
+  auth.user.email 
+    ? <Redirect to='/profile' />
+    : <Redirect to='/login' />
   return (
     <h1>HOME</h1>
   )
